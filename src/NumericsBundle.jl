72f0c52d4db2c53087e4e0e2c1635365ed289d6f
Dan Segal <dansegal2@gmail.com>
2020-04-23 16:10:53 -0700

    Add base list of numerics packages to bundle package

---
+++ src/NumericsBundle.jl
@@ -0,0 +1,13 @@
+module NumericsBundle
+
+  using Reexport
+
+  @reexport using CSV
+  @reexport using DataFrames
+  @reexport using DataStructures
+  @reexport using FFTW
+  @reexport using LinearAlgebra
+  @reexport using Statistics
+  @reexport using StatsBase
+
+end
