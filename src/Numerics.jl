07b650b9f157f1fceb88722aa90f6342f930d492
Dan Segal <dansegal2@gmail.com>
2020-06-04 02:30:56 -0600

    Add Random and SparseArrays to list of Numerics bundled packages

--- src/Numerics.jl
+++ src/Numerics.jl
@@ -5,7 +5,9 @@ module Numerics
   @reexport using DataFrames
   @reexport using DataStructures
   @reexport using LinearAlgebra
+  @reexport using SparseArrays
   @reexport using Statistics
   @reexport using StatsBase
+  @reexport using Random
 
 end
